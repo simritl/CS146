3cfa2df2ba010ed016dee4eba78222c362682df7
simritl <118552067+simritl@users.noreply.github.com>
2024-02-06 14:40:55 -0800

    Add files via upload

---
+++ Homework/Homework2/Bad.java
@@ -0,0 +1,57 @@
+package hw2;
+
+public class Bad {
+	
+	public static boolean isBadVersion(int x) {
+		
+		if (x >= 15) {
+			return true;
+		}
+		return false;
+		
+	}
+	
+	public static int firstBadVersion(int n) {
+		
+		int start = 1; 
+		int end = n; 
+		int middle = 0;
+		 
+		
+		while(start < end) {
+			middle = start +(end - start)/2;
+			
+			boolean y = isBadVersion(middle);
+			
+			if(isBadVersion(middle)) {
+				 end = middle; 
+	
+				
+			}
+			else {
+				start = middle +1;
+			}
+			
+			
+			
+		}
+		return start;
+	
+		
+	}
+	
+	public static void main (String[] args) {
+		int n = 20; 
+		System.out.print(firstBadVersion(n));
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
